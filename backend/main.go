7df7406171a8de6a926e1aaba1b5b3c5d495ba2d
Vanoraco <linkmarsto@gmail.com>
2024-04-29 15:51:16 +0300

    fixed add to cart

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vanoraco/vshop/controllers"
+	"github.com/vanoraco/vshop/database"
+	"github.com/vanoraco/vshop/middleware"
+	"github.com/vanoraco/vshop/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
--- backend/models/models.go
+++ backend/models/models.go
@@ -3,30 +3,77 @@ package models
 import (
 	"time"
 
-	"go.mongodb.org/mongodriver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID            primitive.ObjectID
-	First_Name    *string
-	Last_Name     *string
-	Password      *string
-	Email         *string
-	Phone         *string
-	Token         *string
-	Refresh_Token *string
-	Created_At    time.Time
-	Updated_At    time.Time
-	User_ID
-	UserCart
-	Address_Details
-	Order_Status
+	ID              primitive.ObjectID `json:"_id" bson:"_id"`
+	First_Name      *string            `json:"first_name"`
+	Last_Name       *string            `json:"last_name"`
+	Password        *string            `json:"password"`
+	Email           *string            `json:"email"`
+	Phone           *string            `json:"phone"`
+	Token           *string            `json:"token"`
+	Refresh_Token   *string            `json:"refresh_token"`
+	Created_At      time.Time          `json:"created_at"`
+	Updated_At      time.Time          `json:"updated_at"`
+	User_ID         string             `json:"user_id"`
+	UserCart        []ProductUser      `json:"user_cart" bson:"user_cart"`
+	Address_Details []Address          `json:"address" bson:"address"`
+	Order_Status    []Order            `json:"orders" bson:"orders"`
 }
 
 type Product struct {
-	Product_ID
-	Product_Name
-	Price
-	Rating
-	Image
+	Product_ID   primitive.ObjectID `bson:"_id"`
+	Product_Name *string            `json:"product_name"`
+	Price        *uint64            `json:"price"`
+	Rating       *uint8             `json:"rating"`
+	Image        *string            `json:"image"`
+}
+
+type Shop struct {
+	Shop_ID       primitive.ObjectID `json:"shop_id"`
+	Shop_Name     *string            `json:"shop_name"`
+	Shop_Owner    Owner              `json:"shop_owner"`
+	Shop_Category *string            `json:"shop_category"`
+	Image         *string            `json:"image"`
+	Rating        *uint8             `json:"rating"`
+}
+
+type Owner struct {
+	Owner_ID    primitive.ObjectID `json:"owner_id"`
+	Owner_Name  *string            `json:"owner_name"`
+	Owner_Img   *string            `json:"owner_img"`
+	Owner_Email *string            `json:"owner_email"`
+	Owner_Phone *string            `json:"owner_phone"`
+}
+
+type ProductUser struct {
+	Product_ID   primitive.ObjectID `json:"_id" bson:"_id"`
+	Product_Name *string            `json:"product_name" bson:"product_name"`
+	Price        int                `json:"price" bson:"price"`
+	Rating       *uint              `json:"rating" bson:"rating"`
+	Image        *string            `json:"image" bson:"image"`
+}
+
+type Address struct {
+	Address_ID primitive.ObjectID `json:"_id" bson:"_id"`
+	House      *string            `json:"house" bson:"name"`
+	Street     *string            `json:"street" bson:"street"`
+	City       *string            `json:"city" bson:"city"`
+	Pincode    *string            `json:"pin_code" bson:"pin_code"`
+}
+
+type Order struct {
+	Order_ID       primitive.ObjectID `json:"_id" bson:"_id"`
+	Order_Cart     []ProductUser      `json:"order_cart" bson:"order_cart"`
+	Ordered_At     time.Time          `json:"ordered_at" bson:"ordered_at"`
+	Price          int                `json:"price" bson:"price"`
+	Discount       *int               `json:"discount" bson:"discount"`
+	Payment_Method Payment            `json:"payment_method" bson:"payment_method"`
+}
+
+type Payment struct {
+	Digital bool `json:"digital"`
+	COD     bool `json:"cod"`
 }
